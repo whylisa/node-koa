8b6dd1c73e2696de61af2419319d30a193dccd67
haoyue.wang <843655240@qq.com>
2020-05-22 11:48:46 +0800

    非阻塞IO

--- globs/index.js
+++ globs/index.js
@@ -1,5 +1,12 @@
 const glob = require('glob')
-
-let result = null
-result = glob.sync(__dirname+"/**/*")
-console.log(result)
+// 阻塞io
+// let result = null
+// result = glob.sync(__dirname+"/**/*")
+// console.log(result)
+// 非阻塞io
+console.time()
+glob(__dirname+"/**/*", function(e,r) {
+    console.log(r)
+})
+console.timeEnd()
+console.log(1+1)
