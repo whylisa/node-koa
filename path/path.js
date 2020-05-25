a1abdbe9381e50a429d4afdc433e3082f16a9b2b
haoyue.wang <843655240@qq.com>
2020-05-25 10:24:08 +0800

    path

---
+++ path/path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+// console.log(path.basename("D:\github\node-jk\path/path.js"))
+// console.log(path.basename("D:\github\node-jk\path"))
+// console.log(process.env.PATH)
+// console.log(path.extname("path.js"))
+// console.log(path.parse(__dirname))
+// console.log(path.parse(__filename))
+console.log(process)
